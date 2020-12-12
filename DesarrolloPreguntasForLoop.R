9a74bd0f5c4b1a4872db76738ed310257837ee7d
MariaJoseDF <maria.donosof@utem.cl>
2020-12-11 22:05:49 -0300

    Agregando columna tamanio

--- DesarrolloPreguntasForLoop.R
+++ DesarrolloPreguntasForLoop.R
@@ -19,3 +19,28 @@ pequena_chile <- read.csv(file = "pequena_chile.csv", sep = ";")
 micro_chile <- read.csv(file = "micro_chile.csv", sep = ";")
 medianas_chile <- read.csv(file = "medianas_chile.csv", sep = ";")
 grandes_chile <- read.csv(file = "grandes_chile.csv", sep = ";")
+
+# # # # # # # # # AGREGANDO VARIABLE TAMANIO # # # # # # # # # # # # # # # # # #
+
+#Creando variables de tamaño
+
+micro <- "Micro"
+mediana <- "Mediana"
+pequena <- "Pequena"
+grande <- "Grande"
+
+#Agregando las variables de tamaño en nueva columna
+
+pequena_peru <- cbind(pequena_peru, Tamanio = pequena)
+pequena_chile <- cbind(pequena_chile, Tamanio = pequena)
+pequena_colombia <- cbind(pequena_colombia, Tamanio = pequena)
+micro_chile <- cbind(micro_chile, Tamanio = micro)
+micro_colombia <- cbind(micro_colombia,Tamanio = micro)
+micro_peru <- cbind(micro_peru, Tamanio = micro)
+medianas_chile <- cbind(medianas_chile, Tamanio = mediana)
+medianas_colombia <- cbind(medianas_colombia, Tamanio = mediana)
+medianas_peru <- cbind(medianas_peru, Tamanio = mediana)
+grandes_chile <- cbind(grandes_chile, Tamanio = grande)
+grandes_colombia <- cbind(grandes_colombia, Tamanio = grande)
+grandes_peru <- cbind(grandes_peru, Tamanio = grande)
+
